2eca0bc1011e82063fea9b3cdc74c88ed2fef89d
Reda-Kriate <kriatereda@gmail.com>
2025-09-14 16:28:20 +0100

    add angular front / CORS config

---
+++ angular-front/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import {ProductsComponent} from './products-component/products-component';
+
+export const routes: Routes = [
+  {path:"products" , component:ProductsComponent}
+];
---
+++ angular-front/src/app/products-component/products-component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgForOf, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-products-component',
+  imports: [
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './products-component.html',
+  styleUrl: './products-component.css'
+})
+export class ProductsComponent {
+  products:any;
+  constructor(private http:HttpClient) {
+  }
+  ngOnInit():void{
+    this.http.get("http://localhost:9999/inventory-service/products").subscribe({
+      next : (data)=>{
+        this.products=data;
+        },
+      error : err => {}
+      }
+    )
+  }
+}
